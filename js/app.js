fa7fb697d3526954af68830021d15e0e3d074520
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2020-12-05 14:37:53 -0800

    comment errors

--- js/app.js
+++ js/app.js
@@ -1,63 +1,63 @@
-// const base = "https://api-jasonandyun.herokuapp.com/"
-let login_user = localStorage.getItem('login_user')==undefined? localStorage.getItem('login_user'):false;
-const base = "http://localhost:3000/"
+// const base = "https://api-jasonandyun.herokuapp.com/api/v1/"
+let login_user = localStorage.getItem('login_user') == undefined ? localStorage.getItem('login_user') : false;
+const base = "http://localhost:3000/api/v1/"
 
-const renderingRestaurantInfo_Main  = ()=> {
-    mainPageSetup((result)=> {
+const renderingRestaurantInfo_Main = () => {
+    mainPageSetup((result) => {
         let obj = JSON.parse(result);
-        for(let i=1;i<=obj.length;i++) {
-            let rest_name = "res_name_"+i;
-            let rest_desc = "res_desc_"+i;
-            document.getElementById(rest_name).innerText = obj[i-1].restaurant_name;
-            document.getElementById(rest_desc).innerText = obj[i-1].restaurant_desc;
+        for (let i = 1; i <= obj.length; i++) {
+            let rest_name = "res_name_" + i;
+            let rest_desc = "res_desc_" + i;
+            document.getElementById(rest_name).innerText = obj[i - 1].restaurant_name;
+            document.getElementById(rest_desc).innerText = obj[i - 1].restaurant_desc;
         }
     })
 }
-const mainPageSetup = (callback)=> {
-    let xhttp= new XMLHttpRequest();
+const mainPageSetup = (callback) => {
+    let xhttp = new XMLHttpRequest();
     console.log("mainPageSetup");
     xhttp.open('GET', base, true)
     xhttp.send();
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
+        if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText)
-            callback(this.responseText)        
+            callback(this.responseText)
         }
     }
-    
+
 }
 
-const goToDetailPage =(id) => {
-    if(login_user) {
-        let get_id = id.substring(id.length-1, id.length) 
-        let url = './src/restaurant.html?'+get_id;
+const goToDetailPage = (id) => {
+    if (login_user) {
+        let get_id = id.substring(id.length - 1, id.length)
+        let url = './src/restaurant.html?' + get_id;
         window.location.href = url;
-        console.log(window.location.href);    
-    }else {
+        console.log(window.location.href);
+    } else {
         alert("Please login first")
     }
-    
+
 }
 
 const addRestaurant = () => {
-    let xhttp= new XMLHttpRequest();
-    xhttp.open('POST', base+'api/restaurant/', true)
-    console.log(base+'api/restaurant/')
+    let xhttp = new XMLHttpRequest();
+    xhttp.open('POST', base + 'api/restaurant/', true)
+    console.log(base + 'restaurant/')
     let new_res_name = document.getElementById('new_res_name').value;
-    let new_res_phone =document.getElementById('new_res_phone').value;
-    let new_res_addr =document.getElementById('new_res_addr').value;
-    let new_res_desc =document.getElementById('new_res_desc').value;
+    let new_res_phone = document.getElementById('new_res_phone').value;
+    let new_res_addr = document.getElementById('new_res_addr').value;
+    let new_res_desc = document.getElementById('new_res_desc').value;
     let data = {
         "restaurant_name": new_res_name,
         "restaurant_phone": new_res_phone,
         "restaurant_addr": new_res_addr,
         "restaurant_desc": new_res_desc
     }
-    
+
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.send(JSON.stringify(data))
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
+        if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText)
         }
     }
@@ -65,44 +65,44 @@ const addRestaurant = () => {
 
 const admin_open = () => {
     let password = prompt('Are u a admin? please input 4 digit password')
-    if(password==1234) {
-        document.getElementById("admin_modal_btn").click();        
-    }else {
+    if (password == 1234) {
+        document.getElementById("admin_modal_btn").click();
+    } else {
         alert("You are not authorized. Thanks")
     }
 
 }
 
 
-const delete_restaurant_shows_all =() => {
-    delete_restaurant((result)=> {
+const delete_restaurant_shows_all = () => {
+    delete_restaurant((result) => {
         let res = JSON.parse(result);
         console.log(res.length)
         console.log(res)
-        
-        if(document.getElementById('btn_container_remove')) {
+
+        if (document.getElementById('btn_container_remove')) {
             let temp = document.getElementById('btn_container_remove')
             document.getElementById('remove_detail_modal').removeChild(temp);
         }
 
-        
+
         let btn_container = document.createElement('div');
         btn_container.id = 'btn_container_remove'
-       
-        for(let i =0;i<res.length;i++) {
+
+        for (let i = 0; i < res.length; i++) {
             let btn = document.createElement('Button');
             btn.innerText = res[i]['restaurant_name']
             btn.classList.add('remove_btn')
             btn_container.append(btn);
-            let n =i+1
-            btn.id='btn_'+n;
+            let n = i + 1
+            btn.id = 'btn_' + n;
             btn.addEventListener('click', function() {
                 console.log(this.id);
                 let target = this.id
                 handler_deletion_restaurant(target)
             })
         }
-        
+
         document.getElementById('remove_detail_modal').append(btn_container)
 
         document.getElementById('remove_modal_btn').click();
@@ -110,64 +110,64 @@ const delete_restaurant_shows_all =() => {
 
 }
 
-const delete_restaurant = (callback) => {
+const delete_restaurant = (callback) => { // TODO: UPDATE XHTTP CALL
     let xhttp = new XMLHttpRequest();
-    xhttp.open('GET', base+'api/restaurant/del/all', true )
-    
+    xhttp.open('GET', base + 'restaurant/del/all', true)
+
     xhttp.send()
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
+        if (this.readyState == 4 && this.status == 200) {
             callback(this.responseText)
         }
     }
-   
+
 
 }
 
-const handler_deletion_restaurant  = (id) => {
-    delete_restaurant_by_id(id, (result)=> {
-        
+const handler_deletion_restaurant = (id) => {
+    delete_restaurant_by_id(id, (result) => {
+
     })
-    
+
 }
-const delete_restaurant_by_id =(id, callback)=> {
-    let xhttp= new XMLHttpRequest();
+const delete_restaurant_by_id = (id, callback) => {
+    let xhttp = new XMLHttpRequest();
     console.log("Remove")
     console.log(this);
     // let parameter = window.location.href    
-    let restaurant_id = id.substring(id.length-1, id.length)
+    let restaurant_id = id.substring(id.length - 1, id.length)
 
-    xhttp.open('DELETE', base+'api/restaurant/'+restaurant_id, true)
+    xhttp.open('DELETE', base + 'restaurant/' + restaurant_id, true)
     xhttp.send()
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
+        if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText)
-            callback(this.responseText)        
+            callback(this.responseText)
         }
     }
 }
 
-const login= ()=> {
-    document.getElementById("login_btn").click();        
+const login = () => {
+    document.getElementById("login_btn").click();
 }
 
-const signup= () => {
+const signup = () => {
     document.getElementById('signup_btn').click();
 }
-const login_submit = ()=> {
-    login_handler((result)=> {
-        if(result=='password wrong') {
+const login_submit = () => {
+    login_handler((result) => {
+        if (result == 'password wrong') {
             document.getElementById('login_close_btn').click();
             alert('password wrong')
-        }else {
+        } else {
             document.getElementById('login_close_btn').click();
-            login_user=true;
+            login_user = true;
             localStorage.setItem("login_user", login_user);
             console.log(result);
             console.log(JSON.parse(result))
             console.log(JSON.parse(result)[0])
             let name = JSON.parse(result)[0];
-            document.getElementById('user_name').innerText = "Welcome "+name['userid'];
+            document.getElementById('user_name').innerText = "Welcome " + name['userid'];
 
             alert('Login success')
 
@@ -175,30 +175,30 @@ const login_submit = ()=> {
     })
 }
 
-const login_handler = (callback)=> {
+const login_handler = (callback) => {
     let xhttp = new XMLHttpRequest();
     let userid = document.getElementById('login_id').value;
     let password = document.getElementById('login_password').value;
     console.log(userid)
     console.log(password)
-    xhttp.open('GET', base+'api/user/'+userid+"&"+password);
+    xhttp.open('GET', base + 'user/' + userid + "&" + password);
     xhttp.send()
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
-            
-            callback(this.responseText)        
+        if (this.readyState == 4 && this.status == 200) {
+
+            callback(this.responseText)
         }
     }
 }
 
 
-const check_duplicate_id =() => {
-    duplicate_id_handler((result)=> {
-        console.log(result=="true");
-        if(result=="true") {
+const check_duplicate_id = () => {
+    duplicate_id_handler((result) => {
+        console.log(result == "true");
+        if (result == "true") {
             console.log(result);
             alert("Change your id")
-        }else {
+        } else {
             console.log(result);
             document.getElementById("duplicate_id_check").innerText = "done";
         }
@@ -208,43 +208,43 @@ const duplicate_id_handler = (callback) => {
     let xhttp = new XMLHttpRequest();
     let userid = document.getElementById('signup_id').value;
     console.log(userid)
-    xhttp.open('GET', base+'api/user/signup/'+userid);
+    xhttp.open('GET', base + 'user/signup/' + userid);
     xhttp.send()
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
-            
-            callback(this.responseText)        
+        if (this.readyState == 4 && this.status == 200) {
+
+            callback(this.responseText)
         }
     }
 
 }
-const signup_submit =() => {
+const signup_submit = () => {
     console.log(document.getElementById('duplicate_id_check').innerHTML)
-    if(document.getElementById('duplicate_id_check').innerHTML=="done") {
-        signup_submit_handler((result)=> {
+    if (document.getElementById('duplicate_id_check').innerHTML == "done") {
+        signup_submit_handler((result) => {
             console.log(result);
         })
-    }else {
+    } else {
         alert("Do duplicate check first")
-        
+
     }
 }
 
-const signup_submit_handler = (callback)=> {
+const signup_submit_handler = (callback) => {
     let xhttp = new XMLHttpRequest();
     let userid = document.getElementById('signup_id').value;
     let password = document.getElementById('signup_password').value;
     console.log(userid)
     console.log(password)
-    xhttp.open('POST', base+'api/user/new/signup')
+    xhttp.open('POST', base + 'user/new/signup')
     let data = {
-        "new_id" : userid,
+        "new_id": userid,
         "new_password": password
     }
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.send(JSON.stringify(data))
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
+        if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText)
             callback(this.responseText);
         }
--- js/restaurant.js
+++ js/restaurant.js
@@ -1,11 +1,11 @@
-// const base = "https://api-jasonandyun.herokuapp.com/"
+// const base = "https://api-jasonandyun.herokuapp.com/api/v1/"
 let user_login = localStorage.getItem('login_user')
 localStorage.setItem('login_user', 'true')
 console.log(user_login)
-const base = "http://localhost:3000/api/"
+const base = "http://localhost:3000/api/v1/"
 
-const renderingRestaurantInfo_detail  = ()=> {
-    detailPageSetup((result)=> {
+const renderingRestaurantInfo_detail = () => {
+    detailPageSetup((result) => {
         let res = JSON.parse(result)
         let res_name = document.getElementById('res_name_detail');
         let res_desc = document.getElementById('res_desc_detail');
@@ -18,11 +18,11 @@ const renderingRestaurantInfo_detail  = ()=> {
         res_addr.innerText = res[0]['restaurant_addr']
 
     })
-    detailPageSetup_menu((result)=> {
+    detailPageSetup_menu((result) => {
         console.log(result);
         let res = JSON.parse(result);
         console.log(res.length)
-        for(let i =0;i<res.length;i++) {
+        for (let i = 0; i < res.length; i++) {
             let node = document.createElement('div');
             node.classList.add('menu_box')
 
@@ -34,75 +34,75 @@ const renderingRestaurantInfo_detail  = ()=> {
             menu_item.classList.add('menu_item')
             menu_price.classList.add('menu_price')
             menu_description.classList.add('menu_description')
-            menu_description.innerText= res[i]['menudescription']
-            
-            
+            menu_description.innerText = res[i]['menudescription']
+
+
             node.appendChild(menu_item)
             node.appendChild(menu_price)
             node.appendChild(menu_description)
-            
-            
 
-            let menu_container= document.getElementById('menu_container');
+
+
+            let menu_container = document.getElementById('menu_container');
             menu_container.appendChild(node);
         }
 
     })
-  
-    
+
+
 }
 
-const detailPageSetup = (callback)=> {
-    let xhttp= new XMLHttpRequest();
+const detailPageSetup = (callback) => {
+    let xhttp = new XMLHttpRequest();
     console.log("detailPage");
-    
-    let parameter = window.location.href    
-    let restaurant_id = parameter.substring(parameter.length-1, parameter.length)
+
+    let parameter = window.location.href
+    let restaurant_id = parameter.substring(parameter.length - 1, parameter.length)
     console.log(restaurant_id)
-    console.log(base+'restaurant/'+restaurant_id)
-    xhttp.open('GET', base+'restaurant/'+restaurant_id, true)
+    console.log(base + 'restaurant/' + restaurant_id)
+    xhttp.open('GET', base + 'restaurant/' + restaurant_id, true)
     xhttp.send();
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
+        if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText)
-            callback(this.responseText)        
+            callback(this.responseText)
         }
     }
-    
+
 }
 
-const detailPageSetup_menu = (callback)=> {
-    let xhttp= new XMLHttpRequest();    
-    let parameter = window.location.href    
-    let restaurant_id = parameter.substring(parameter.length-1, parameter.length)
-    xhttp.open('GET', base+'menu/'+restaurant_id, true)
+const detailPageSetup_menu = (callback) => {
+    let xhttp = new XMLHttpRequest();
+    let parameter = window.location.href
+    let restaurant_id = parameter.substring(parameter.length - 1, parameter.length)
+    xhttp.open('GET', base + 'menu/' + restaurant_id, true)
     xhttp.send();
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
+        if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText)
-            callback(this.responseText)        
+            callback(this.responseText)
         }
     }
-    
+
 }
 
 const add_menu = () => {
-    add_menu_handler((result)=> {
+    add_menu_handler((result) => {
         console.log(result)
     })
 }
 
 const add_menu_handler = (callback) => {
-    let xhttp=new XMLHttpRequest();   
+    let xhttp = new XMLHttpRequest();
     let parameter = window.location.href;
-    let restaurant_id = parameter.substring(parameter.length-1, parameter.length);
-    xhttp.open('POST', base+'menu/'+restaurant_id, true)
+    let restaurant_id = parameter.substring(parameter.length - 1, parameter.length);
+    xhttp.open('POST', base + 'menu/' + restaurant_id, true)
 
     let new_menu_name = document.getElementById('new_menu_name').value;
-    let new_menu_amount =document.getElementById('new_menu_amount').value;
-    let new_menu_desc =document.getElementById('new_menu_desc').value;    
+    let new_menu_amount = document.getElementById('new_menu_amount').value;
+    let new_menu_desc = document.getElementById('new_menu_desc').value;
     let data = {
-        "restaurant_id":restaurant_id,
+        "restaurant_id": restaurant_id,
         "menu_name": new_menu_name,
         "menu_amount": new_menu_amount,
         "menu_desc": new_menu_desc
@@ -111,77 +111,76 @@ const add_menu_handler = (callback) => {
     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhttp.send(JSON.stringify(data))
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
+        if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText)
-            callback(this.responseText)        
+            callback(this.responseText)
         }
     }
 }
 
 const delete_menu = () => {
     console.log("delete menu")
-    delete_menu_show_all_handler((res)=> {
-        res  = JSON.parse(res)
+    delete_menu_show_all_handler((res) => {
+        res = JSON.parse(res)
         console.log(res)
-        if(document.getElementById('btn_container_remove_menu')) {
+        if (document.getElementById('btn_container_remove_menu')) {
             let temp = document.getElementById('btn_container_remove_menu')
             document.getElementById('remove_detail_modal_menu').removeChild(temp);
         }
 
-        
+
         let btn_container = document.createElement('div');
         btn_container.id = 'btn_container_remove_menu'
-        
-        for(let i =0;i<res.length;i++) {
+
+        for (let i = 0; i < res.length; i++) {
             let btn = document.createElement('Button');
             btn.innerText = res[i]['items']
             btn.classList.add('remove_btn')
             btn_container.append(btn);
-            btn.id='btn_'+res[i]['menuid'];
+            btn.id = 'btn_' + res[i]['menuid'];
             btn.addEventListener('click', function() {
                 console.log(this.id);
                 let target = this.id
                 delete_menu_by_id(target);
             })
-        }    
+        }
         document.getElementById('remove_detail_modal_menu').append(btn_container)
 
-     
+
 
     })
 }
-const delete_menu_show_all_handler = (callback) => {
-    let xhttp=new XMLHttpRequest();   
+const delete_menu_show_all_handler = (callback) => { // TODO UPDATE XHTTP CALL /menu/all/:restaurantid
+    let xhttp = new XMLHttpRequest();
     let parameter = window.location.href;
-    let restaurant_id = parameter.substring(parameter.length-1, parameter.length);
-    xhttp.open('GET', base+'menu/del/all/'+restaurant_id, true)
+    let restaurant_id = parameter.substring(parameter.length - 1, parameter.length);
+    xhttp.open('GET', base + 'menu/del/all/' + restaurant_id, true)
     xhttp.send()
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
-            
-            callback(this.responseText)        
+        if (this.readyState == 4 && this.status == 200) {
+
+            callback(this.responseText)
         }
     }
 
 }
 
 const delete_menu_by_id = (target) => {
-    delete_menu_by_id_handler(target, (result)=> {
+    delete_menu_by_id_handler(target, (result) => {
         console.log(result);
     })
 }
 const delete_menu_by_id_handler = (target, callback) => {
     console.log(target);
     xhttp = new XMLHttpRequest();
-    let parameter = target.substring(target.length-1, target.length);
-    xhttp.open('DELETE', base+'menu/'+parameter, true);
+    let parameter = target.substring(target.length - 1, target.length);
+    xhttp.open('DELETE', base + 'menu/' + parameter, true);
     xhttp.send();
     xhttp.onreadystatechange = function() {
-        if(this.readyState==4 && this.status ==200) {
-            callback(this.responseText)        
+        if (this.readyState == 4 && this.status == 200) {
+            callback(this.responseText)
         }
     }
 }
 
 window.onpageshow = renderingRestaurantInfo_detail;
-
